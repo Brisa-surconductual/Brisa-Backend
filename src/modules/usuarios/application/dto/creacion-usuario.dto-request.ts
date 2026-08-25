86cea93b01d8774e88dcea87615d771ed31b37eb
SamuelPR21 <u20222208795@usco.edu.co>
2026-08-24 21:47:25 -0500

    Fix creacion de cuenta- fecha de nacimeinto

--- src/modules/usuarios/application/dto/creacion-usuario.dto-request.ts
+++ src/modules/usuarios/application/dto/creacion-usuario.dto-request.ts
@@ -1,6 +1,13 @@
-import { IsEmail, MinLength, IsDate, IsBoolean, IsString, IsNumber, IsEnum } from 'class-validator';
+import { IsEmail, MinLength, IsDate, IsString, IsNumber, IsEnum } from 'class-validator';
+import { Transform } from 'class-transformer';
 import { NivelAcademico } from '../../domain/enums/nivel-academico-enum';
 import { MotivoConsumo } from '../../domain/enums/motivo-consumo-enum';
+
+function toDateWithMidnight({ value }: { value: unknown }): Date | undefined {
+    if (typeof value !== 'string') return undefined;
+    const soloFecha = /^\d{4}-\d{2}-\d{2}$/.test(value);
+    return new Date(soloFecha ? `${value}T00:00:00.000Z` : value);
+}
 
 export class CreacionUsuarioDtoRequest {
     @IsEmail()
@@ -9,6 +16,7 @@ export class CreacionUsuarioDtoRequest {
     @MinLength(8)
     contrasena!: string;
 
+    @Transform(toDateWithMidnight)
     @IsDate()
     fechaNacimiento!: Date;
 
@@ -27,9 +35,11 @@ export class CreacionUsuarioDtoRequest {
     @IsEnum(NivelAcademico)
     nivelAcademico!: NivelAcademico;
 
+    @Transform(toDateWithMidnight)
     @IsDate()
     fechaInicioConsumo!: Date;
 
+    @Transform(toDateWithMidnight)
     @IsDate()
     fechaUltimoConsumo!: Date;
 
