d0756ecffe252511b07f962ab947c4489084e8ec
SamuelPR21 <u20222208795@usco.edu.co>
2026-08-30 17:25:10 -0500

    Inicios de programar disponibilidad de contenido

---
+++ src/modules/cronograma/application/use-cases/programar-disponibilidad-contenido.use-case.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class ProgramarDisponibilidadContenidoUseCase {
+    constructor(
+
+    ){}
+
+    async execute(dato: any): Promise<any> {
+    
+    
+    }
+}
