42d0c4391b28e59595847c44b20d6ac40bb57f05
Leandro <u20222207567@usco.edu.co>
2026-09-09 10:19:04 -0500

    fix(cronograma): usar imports relativos en calendario

--- src/modules/cronograma/application/dto/contenido/obtener-contenidos-unidad.dto-response.ts
+++ src/modules/cronograma/application/dto/contenido/obtener-contenidos-unidad.dto-response.ts
@@ -1,27 +1,30 @@
-import { IsEnum, IsString } from 'class-validator';
-import { ContenidoDetalleResponseDto } from '../contenido/contenido-detalle.dto-response';
-import { EstadoContenido } from '../../../domain/enums/estado-contenido.enum';
-import { EstadoCronograma } from '../../../domain/enums/estado-cronograma.enum';
+import { IsEnum, IsString } from "class-validator";
+import {ContenidoDetalleResponseDto} from "../contenido/contenido-detalle.dto-response";
+import { EstadoContenido } from "../../../domain/enums/estado-contenido.enum";
+import {EstadoCronograma} from "../../../domain/enums/estado-cronograma.enum";
+
 
 export class ObtenerContenidosUnidadDtoResponse {
-  @IsString()
-  cronogramaId!: string;
+    @IsString()
+    cronogramaId!: string;
 
-  @IsString()
-  unidadTemporalId!: string;
+    @IsString()
+    unidadTemporalId!: string;
 
-  @IsString()
-  @IsEnum(EstadoCronograma)
-  estadoCronograma!: string;
+    @IsString()
+    @IsEnum(EstadoCronograma)
+    estadoCronograma!: string;
 
-  @IsString()
-  @IsEnum(EstadoContenido)
-  estadoUnidadTemporal!: string;
+    @IsString()
+    @IsEnum(EstadoContenido)
+    estadoUnidadTemporal!: string;
 
-  @IsString()
-  contenidos!: ContenidoDetalleResponseDto[];
+    @IsString()
+    contenidos!: ContenidoDetalleResponseDto[];
 
-  constructor(data: Partial<ObtenerContenidosUnidadDtoResponse>) {
-    Object.assign(this, data);
-  }
+
+
+    constructor(data: Partial<ObtenerContenidosUnidadDtoResponse>) {
+        Object.assign(this, data);
+    }
 }
--- src/modules/cronograma/application/use-cases/cronograma/cronograma-calendario.use-case.ts
+++ src/modules/cronograma/application/use-cases/cronograma/cronograma-calendario.use-case.ts
@@ -5,68 +5,60 @@ import { CronogramaCalendarioDtoRequest } from '../../dto/cronograma/cronograma-
 import { ObtenerContenidosUnidadDtoResponse } from '../../dto/contenido/obtener-contenidos-unidad.dto-response';
 import { UnidadTemporalNoEncontradaException } from '../../../domain/exeption/unidades-temporales/unidad-temporal-no-encotrada.exception';
 import { UnidadTemporalNoPerteneceACronogramaException } from '../../../domain/exeption/unidades-temporales/unidad-temporal-no-pertence-cronograma.expetion';
-import { CronogramaNoEncontradoException } from '../../../domain/exeption/cronograma/cronograma-no-encontrado.exeption';
+import {CronogramaNoEncontradoException} from "../../../domain/exeption/cronograma/cronograma-no-encontrado.exeption";
 import { CronogramaRepository } from '../../../domain/repositories/cronograma.repository';
-import { UnidadTemporal } from '../../../domain/entities/unidad-temporal.entity';
+import {UnidadTemporal} from "../../../domain/entities/unidad-temporal.entity";
 
 @Injectable()
 export class CronogramaCalendarioUseCase {
-  constructor(
-    private readonly unidadTemporalRepository: UnidadTemporalRepository,
-    private readonly contenidoCronogramaRepository: ContenidoCronogramaRepository,
-    private readonly cronogramaRepository: CronogramaRepository,
-  ) {}
+    
+    constructor(
+        private readonly unidadTemporalRepository: UnidadTemporalRepository,
+        private readonly contenidoCronogramaRepository: ContenidoCronogramaRepository,
+        private readonly cronogramaRepository: CronogramaRepository,
+    ) {}
 
-  async execute(
-    request: CronogramaCalendarioDtoRequest,
-  ): Promise<ObtenerContenidosUnidadDtoResponse> {
-    const cronograma = await this.cronogramaRepository.buscarPorId(
-      request.idCronograma,
-    );
-    if (!cronograma) {
-      throw new CronogramaNoEncontradoException();
+    async execute(request: CronogramaCalendarioDtoRequest): Promise<ObtenerContenidosUnidadDtoResponse> {
+
+        const cronograma = await this.cronogramaRepository.buscarPorId(request.idCronograma);
+        if (!cronograma) {
+            throw new CronogramaNoEncontradoException();
+        }
+
+        const unidadTemporal = await this.unidadTemporalRepository.obtenerPorIdUnidadTemporal(request.idUnidadTemporal);
+        if (!unidadTemporal) {
+            throw new UnidadTemporalNoEncontradaException();
+        }
+
+        if (unidadTemporal.id_cronograma !== cronograma.id_cronograma) {
+            throw new UnidadTemporalNoPerteneceACronogramaException();
+        }
+
+        const relacionesDB = await this.contenidoCronogramaRepository.obtenerContenidosConRecursosPorUnidadTemporal(request.idUnidadTemporal);
+
+        const contenidosMapeados = relacionesDB.map(relacion => {
+            const contenido = relacion.contenidos;
+            return {
+                idContenido: contenido.id_contenido,
+                nombreContenido: contenido.nombre_contenido,
+                tipo: contenido.tipo_contenido,
+                recursos: contenido.recursos_contenido.map(recurso => ({
+                    idRecurso: recurso.id_recurso,
+                    claveAlmacenamiento: recurso.clave_almacenamiento,
+                }))
+            };
+        });
+
+       const estadoCronograma = cronograma.estado;
+       
+       const estadoUnidadTemporal = unidadTemporal.obtenerEstado();
+
+        return new ObtenerContenidosUnidadDtoResponse({
+            cronogramaId: request.idCronograma,
+            unidadTemporalId: request.idUnidadTemporal,
+            estadoCronograma: estadoCronograma,
+            estadoUnidadTemporal: estadoUnidadTemporal,
+            contenidos: contenidosMapeados
+        });
     }
-
-    const unidadTemporal =
-      await this.unidadTemporalRepository.obtenerPorIdUnidadTemporal(
-        request.idUnidadTemporal,
-      );
-    if (!unidadTemporal) {
-      throw new UnidadTemporalNoEncontradaException();
-    }
-
-    if (unidadTemporal.id_cronograma !== cronograma.id_cronograma) {
-      throw new UnidadTemporalNoPerteneceACronogramaException();
-    }
-
-    const relacionesDB =
-      await this.contenidoCronogramaRepository.obtenerContenidosConRecursosPorUnidadTemporal(
-        request.idUnidadTemporal,
-      );
-
-    const contenidosMapeados = relacionesDB.map((relacion) => {
-      const contenido = relacion.contenidos;
-      return {
-        idContenido: contenido.id_contenido,
-        nombreContenido: contenido.nombre_contenido,
-        tipo: contenido.tipo_contenido,
-        recursos: contenido.recursos_contenido.map((recurso) => ({
-          idRecurso: recurso.id_recurso,
-          claveAlmacenamiento: recurso.clave_almacenamiento,
-        })),
-      };
-    });
-
-    const estadoCronograma = cronograma.estado;
-
-    const estadoUnidadTemporal = unidadTemporal.obtenerEstado();
-
-    return new ObtenerContenidosUnidadDtoResponse({
-      cronogramaId: request.idCronograma,
-      unidadTemporalId: request.idUnidadTemporal,
-      estadoCronograma: estadoCronograma,
-      estadoUnidadTemporal: estadoUnidadTemporal,
-      contenidos: contenidosMapeados,
-    });
-  }
 }
